04f5835e0141daf29c20082673ecd7c13433923e
anikethsanthan <anikethsanthan999@gmail.com>
2024-12-01 16:56:42 +0530

    Made apis for the app

--- src/app.js
+++ src/app.js
@@ -4,15 +4,9 @@ const User =require("./models/user");
 
 
 const app=express()
-
+app.use(express.json());
 app.post("/signup",async (req,res)=>{
-    const user= new User({
-        firstName:"Virat",
-        lastName:"Kohli",
-        emailId:"Virat@213",
-        password:"virat@123",
-        age:45,
-    })
+    const user= new User(req.body)
   
 try{
     await user.save();
@@ -20,9 +14,64 @@ try{
 }catch{
     res.status(400).send("Unsuccesfull request");
 }
-
-    
+   
 })
+app.get("/user", async(req,res)=>{
+    const userEmail= req.body.emailId;
+try{
+    const users =  await User.find({emailId:userEmail})
+    if(users.length ===0){
+        res.send("User not found");
+         
+    }else{
+        res.send(users);
+        
+    }
+   
+}catch{
+    res.status(400).send("something went wrong");
+}
+
+})
+
+app.get("/feed", async(req,res)=>{
+    try{
+        const users= await User.find({});
+        if(!users){
+            res.status(400).send("User not found");
+        }else{
+            res.send(users);
+        }
+        
+    }catch{
+        res.status(400).send("Something went wrong")
+    }
+})
+
+app.delete("/user", async(req,res)=>{
+    const userId= req.body.userId;
+    try{
+        await User.findByIdAndDelete(userId);
+        res.send("User deleted succesfully")
+    }catch{
+        res.status(400).send("something went wrong")
+    }
+})
+
+app.patch("/user",async(req,res)=>{
+    const userId= req.body.userId;
+    const data= req.body;
+
+    try{
+        await User.findByIdAndUpdate(userId, data);
+        res.send("User updated succesfully");
+    }catch{
+        res.send("something went wrong");
+    }
+})
+
+// app.get("/user",async(req,res)=>{})
+
 connectDb().then(()=>{
     console.log("Connection established to database");
     app.listen(7777,()=>{
@@ -31,7 +80,7 @@ connectDb().then(()=>{
     
 }
     ).catch(()=>{
-        console.log("Connection was unsuccessfull with the database")
+        console.log(" unsuccessfull connection with the database")
     }
     
 )
