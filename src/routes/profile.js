bb289723ff15d5e2df79f45aa4187c70b5bbca5e
anikethsanthan <anikethsanthan999@gmail.com>
2024-12-03 13:45:24 +0530

    added profile edit api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -3,6 +3,7 @@ const profileRouter= express.Router();
 const {userAuth}=require("../middlewares/auth");
 const User =require("../models/user");
 const validator =require("validator");
+const {validateEditRequest}=require("../utils/validateSignup")
 
 profileRouter.get("/profile/view",userAuth, async(req,res)=>{
     try{
@@ -17,25 +18,28 @@ profileRouter.get("/profile/view",userAuth, async(req,res)=>{
     }
 })
 
-profileRouter.patch("/profile/edit/:userId",async(req,res)=>{
-    const userId= req.params?.userId;
-    const data= req.body;
-    try{
-        const allowedUpdates=["firstName","lastName","password","gender","about","skills","photoUrl"];
-        const isUpdateAllowed= Object.keys(data).every((k)=> allowedUpdates.includes(k));
+profileRouter.patch("/profile/edit",userAuth,async(req,res)=>{
+    
+ 
 
-        if(!isUpdateAllowed){
-            throw new Error("Updation of this field is not allowed")
+    try{
+        if(!validateEditRequest(req)){
+            throw new Error("Invalid Edit reques")
         }
+        const user= req.user;
+ 
+        Object.keys(req.body).forEach((key)=>(user[key]=req.body[key ]))
+       
+        const data= req.body;
         if(data.skills && data.skills.length > 10){
             throw new Error("You can only add upto 10 skills")
         }
         if(data.photoUrl && !validator.isURL(data.photoUrl)){
-            throw new Error ("Enter a valid photo URL")
+            throw new Error ('Enter a valid photo URL')
         }
 
-        await User.findByIdAndUpdate(userId, data ,{runValidators:true});
-        res.send("User updated succesfully");
+        await user.save();
+        res.send( user.firstName+' your profile  updated succesfully');
     }catch(err){
         res.send("something went wrong"+ " "+err.message);
     }
--- src/utils/validateSignup.js
+++ src/utils/validateSignup.js
@@ -2,16 +2,29 @@ const validator=require("validator");
 
 const validateSignup=(req)=>{
 const {firstName, lastName, emailId, password}=req.body;
+try{
+    if(!firstName||!lastName){
+        throw new Error("Name is a required field to proceed")
+    }
+    if(!validator.isEmail(emailId)){
+        throw new Error("Enter a valid email id")
+    }
+    if(!validator.isStrongPassword(password)){
+        throw new Error(" Weak password ! Please try a strong password")
+    }
+}catch(err){
+    throw new Error("Error in validation process")
+}
 
-if(!firstName||!lastName){
-    throw new Error("Name is a required field to proceed")
-}
-if(!validator.isEmail(emailId)){
-    throw new Error("Enter a valid email id")
-}
-if(!validator.isStrongPassword(password)){
-    throw new Error(" Weak password ! Please try a strong password")
-}
 }
 
-module.exports={validateSignup}
+const validateEditRequest =(req)=>{
+    const allowedUpdates=["firstName","lastName","gender","about","skills","photoUrl"];
+    const iseditAllowed = Object.keys(req.body).every((field)=>allowedUpdates.includes(field));
+    return iseditAllowed;
+}
+
+
+
+
+module.exports={validateSignup, validateEditRequest}
