b2e34ce28a48978c1ff18ca7b0003cd75e79f05d
anikethsanthan <anikethsanthan999@gmail.com>
2024-12-12 20:05:24 +0530

    added cors

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ const User =require("./models/user");
 
 
 const cookieparser=require("cookie-parser");
+const cors=require("cors");
 
 
 
 
 const app=express()
+app.use(cors())
 app.use(express.json());
 app.use(cookieparser());
 
