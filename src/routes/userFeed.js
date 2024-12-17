693676d28321452c594c90c11eb632eb114bf6d6
anikethsanthan <anikethsanthan999@gmail.com>
2024-12-17 20:09:52 +0530

    Feed api made and pagination and validations are added

--- src/routes/userFeed.js
+++ src/routes/userFeed.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const { userAuth } = require("../middlewares/auth")
 const ConnectRequest=require("../models/connectionRequest");
+const User =require("../models/user");
 
 const SAFE_DATA= ["firstName","lastName","age","gender","about","skills","photoUrl"]
 const userFeedRouter =express.Router()
@@ -52,6 +53,44 @@ userFeedRouter.get("/user/connections", userAuth, async (req,res)=>{
 
     }catch(err){
         res.status(400).send("Error :"+ err.message)
+    }
+})
+
+userFeedRouter.get("/feed", userAuth, async(req,res)=>{
+    try{
+
+        const loggedInUSer=req.user;
+        const page = parseInt(req.query.page)||1;
+        let limit= parseInt(req.query.limit)||10;
+        limit =limit>50? 50 :limit;
+        const skip = (page-1)*limit;
+
+
+        const connectionRequest= await ConnectRequest.find({
+            $or:[
+                {fromUserId: loggedInUSer._id},
+                {toUserId:loggedInUSer._id}
+            ]
+        }).select("fromUserId toUserId")
+
+        const hiddenUsersFromFedd= new Set();
+
+        connectionRequest.forEach((req)=>{
+            hiddenUsersFromFedd.add(req.fromUserId.toString());
+            hiddenUsersFromFedd.add(req.toUserId.toString());
+        })
+
+        const users= await User.find({
+            $and:[
+                {_id:{$nin: Array.from(hiddenUsersFromFedd)}},
+                {_id:{$ne: loggedInUSer._id}}
+            ]
+        }).select(SAFE_DATA).skip(skip).limit(limit)
+
+        res.status(200).send(users)
+
+    }catch(err){
+        res.status(400).send("Error :" + err.message)
     }
 })
 
