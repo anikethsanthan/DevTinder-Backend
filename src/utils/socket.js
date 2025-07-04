15fb94d8fbe042aea2266edac2598bc77406f1d7
anikethsanthan <anikethsanthan999@gmail.com>
2025-07-04 18:38:00 +0530

    CHanges

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -10,8 +10,8 @@ const initialiseSocket = (server) => {
         "https://dev-tinder-frontend-pi.vercel.app",
       ],
       credentials: true,
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-      allowedHeaders: ["Content-Type", "Authorization"],
+      // methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      // allowedHeaders: ["Content-Type", "Authorization"],
     },
   });
 
