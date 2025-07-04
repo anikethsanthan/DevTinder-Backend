d6116c99b6e301a050a29ed51fa8d2ce21abd422
anikethsanthan <anikethsanthan999@gmail.com>
2025-07-04 11:14:22 +0530

    Added send mail api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,68 +1,80 @@
 const express = require("express");
-const {validateSignup} =require("../utils/validateSignup");
-const bcrypt=require("bcrypt");
-const User =require("../models/user");
-const getJWT=require("../models/user.js");
-const validatePassword= require("../models/user")
+const { validateSignup } = require("../utils/validateSignup");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const getJWT = require("../models/user.js");
+const validatePassword = require("../models/user");
+const sendMail = require("../utils/sendMail.js");
 
+const authRouter = express.Router();
+authRouter.post("/signup", async (req, res) => {
+  try {
+    validateSignup(req);
+    const { firstName, lastName, emailId, password } = req.body;
 
-const authRouter= express.Router();
-    authRouter.post("/signup",async (req,res)=>{  
-    try{
-        validateSignup(req);
-        const {firstName, lastName, emailId, password}=req.body;
-    
-        const passwordHash=await bcrypt.hash(password,10);
-        const user= new User({
-            firstName,
-            lastName,
-            emailId,
-            password:passwordHash
-        })
-        const token= await user.getJWT()
-        if(!token){
-            throw new Error ("Invalid token")
-        }
-        await user.save();
-        res.cookie("token",token)
-        res.send(user);
-    }catch(err){
-        res.status(400).send("ERROR :"+" "+err.message);
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+    const token = await user.getJWT();
+    if (!token) {
+      throw new Error("Invalid token");
     }
-       
-    })
+    await user.save();
+    res.cookie("token", token);
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("ERROR :" + " " + err.message);
+  }
+});
 
-    authRouter.post("/login", async(req,res)=>{
-        try{
-            const {emailId,password}=req.body;
-    
-            const user= await User.findOne({emailId:emailId});
-            if(!user){
-                throw new Error("Invalid Credentials")
-            }
-            const token= await user.getJWT()
-            if(!token){
-                throw new Error ("Invalid token")
-            }
-            const isPasswordValid= await user.validatePassword(password)
-            if(!isPasswordValid){
-                throw new Error("Invalid Credentials")
-            }else{
-                res.cookie("token",token)
-                res.send(user)
-            }
-           
-            
-    
-        }catch(err){
-            res.status(400).send("Error:"+ " "+err.message); 
-        }
-    })
+authRouter.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
 
-    authRouter.post("/logout",async(req,res)=>{
-        res.cookie("token",null,{
-            expires:new Date(Date.now())
-        })
-        res.send("Logged Out");
-    })
-module.exports= {authRouter};
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid Credentials");
+    }
+    const token = await user.getJWT();
+    if (!token) {
+      throw new Error("Invalid token");
+    }
+    const isPasswordValid = await user.validatePassword(password);
+    if (!isPasswordValid) {
+      throw new Error("Invalid Credentials");
+    } else {
+      res.cookie("token", token);
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Error:" + " " + err.message);
+  }
+});
+
+authRouter.post("/logout", async (req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+  });
+  res.send("Logged Out");
+});
+
+authRouter.post("/sendMail", async (req, res) => {
+  try {
+    const { email, subject, content } = req.body;
+    if (!email || !subject || !content) {
+      throw new Error("Email, subject, and content are required");
+    }
+    const info = await sendMail(email, subject, content);
+    res.status(200).send({
+      message: "Email sent successfully",
+      info: info,
+    });
+  } catch (err) {
+    res.status(400).send("Error:" + " " + err.message);
+  }
+});
+module.exports = { authRouter };
