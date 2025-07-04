5fc6f908be33636c186c01c8b4aa11cef25b7e2c
anikethsanthan <anikethsanthan999@gmail.com>
2025-07-04 18:10:10 +0530

    Added config

--- src/app.js
+++ src/app.js
@@ -16,9 +16,10 @@ app.use(
       "http://localhost:5173",
       "https://my-portfolio-1f2d5.web.app",
       "https://dev-tinder-frontend-pi.vercel.app",
-      "https://dev-tinder-frontend-y6v33r3bw-aniketh-santhans-projects.vercel.app",
     ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
